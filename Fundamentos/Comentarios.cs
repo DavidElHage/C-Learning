7dd321abc454e232c6aac98251b81a75cb7d58e9
DavidElHage <davidelhagedeoliveira@gmail.com>
2025-11-02 22:52:28 -0300

    Adição de Arquivos

---
+++ Fundamentos/Comentarios.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CursoCSharp.Fundamentos {
+    internal class Comentarios {
+
+        /// <summary>
+        /// 
+        /// </summary>
+
+        public static void Executar() {
+            // Cuidado com os comentários desnecessário
+            Console.WriteLine("Código claro é sempre melhor!");
+
+            /* 
+              Comentários
+              de multiplas linhas..
+             */
+            Console.WriteLine("O C# tem o XML Comments");
+        }
+    }
+}
