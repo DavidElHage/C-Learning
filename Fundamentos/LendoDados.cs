c423b7069fdaf80f0ea339c5ac689d64950c2892
DavidElHage <davidelhagedeoliveira@gmail.com>
2025-11-10 23:18:48 -0300

    Atualizações de Exercicios 10/11

---
+++ Fundamentos/LendoDados.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+
+namespace CursoCSharp.Fundamentos {
+    internal class LendoDados {
+        public static void Executar() {
+            Console.Write("Qual é o seu nome? ");
+            string nome = Console.ReadLine();
+
+            Console.WriteLine("Qual é a sua idade? ");
+            int idade = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Qual é o seu Salário?");
+            double salario = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine($"{nome} , {idade} anos , Salário R${salario}");
+
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -11,6 +11,11 @@ namespace CursoCSharp {
                 {"Comentários - Fundamentos", Comentarios.Executar},
                 {"Váriaveis e Constantes - Fundamentos", VariaveisEConstantes.Executar},
                 {"Inferencia - Fundamentos", Inferencia.Executar},
+                {"Interpolacao - Fundamentos", Interpolacao.Executar},
+                {"Notacao Ponto - Fundamentos", NotacaoPonto.Executar},
+                {"Lendo Dados - Fundamentos", LendoDados.Executar},
+                {"Formato dos Números - Fundamentos", FormatoNumeros.Executar},
+                {"Conversoes dos Números - Fundamentos", Conversoes.Executar},
 
             });
 
